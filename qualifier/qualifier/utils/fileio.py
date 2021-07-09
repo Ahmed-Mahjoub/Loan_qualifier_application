e18ef6206cfafad265c969d12dec2c1235bce251
Ahmed-Mahgoub <ahmedelkarar9@gmail.com>
2021-07-08 22:10:58 -0700

    add new

---
+++ qualifier/qualifier/utils/fileio.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+from pathlib import Path
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+# function named save_csv() that uses the csv library to save the qualifying data as a file
+
+def save_csv(output_path, qualifying_loans):
+    header= ("bank_data", "credit_score", "debt", "income", "loan", "home_value")
+    output_path =Path("data/qualifing_loan.csv")
+    with open(output_path, "w") as csvfile:
+      csvwriter = csv.writer(csvfile, delimiter = "|")
+      csvwriter.writerow(header)
+      for item in qualifying_loans:
+        csvwriter.writerow(item)
+
+    return
